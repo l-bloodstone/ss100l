74f1384c4ff964dd386d0c5907ee6738a244c911
Lucas Bloodstone <lucas.bloodstone@outlook.com>
2025-08-28 18:55:22 +0600

    feat: show status code after a failed command

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
+//          ╭─────────────────────────────────────────────────────────╮
+//          │ author : l-bloodstone                                   │
+//          │ email  : lucas.bloodstone@outlook.com                   │
+//          ╰─────────────────────────────────────────────────────────╯
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
 
+// Prints a prompt with a '$' sign
 char* prompt(){
     printf("$ ");
     char* line = NULL;
@@ -12,7 +17,9 @@ char* prompt(){
     return line;
 }
 
+// Tokenize the args list by whitespaces
 char** tokenize_line(char* line) {
+    // hardcoded 'exit' shell builtin command
     int s = strcmp(line, "exit\n");
     if (s == 0) {
         exit(EXIT_SUCCESS);
@@ -31,6 +38,7 @@ char** tokenize_line(char* line) {
             break;
         }
         tokens[i] = token;
+        // reallocate the buffer if needed
         if (i == TOKENS_BUFFER_SIZE - 1) {
             tokens = realloc(tokens, TOKENS_BUFFER_SIZE * 2);
             if (!tokens) {
@@ -43,15 +51,30 @@ char** tokenize_line(char* line) {
     return tokens;
 }
 
-void execute_command(char** args) {
+int execute_command(char** args) {
+    int fd[2];
+    // Creating a pipe to pass status code from child to parent
+    pipe(fd);
     int pid = fork();
     if (pid < 0) {
         perror("Fork failed!!");
+        close(fd[0]);
+        close(fd[1]);
     }
     if (pid == 0) {
-        execvp(*args, args);
+        close(fd[0]);
+        int exit_status = execvp(args[0], args);
+        write(fd[1], (void*)&exit_status, 4);
+        close(fd[1]);
+        // explicit exit from child process
+        exit(exit_status);
     } else {
+        close(fd[1]);
         wait(NULL);
+        int status = 0;
+        read(fd[0], (void*)&status, 4);
+        close(fd[0]);
+        return status;
     }
 }
 
@@ -59,7 +82,11 @@ void shell_loop(){
     while (1) {
         char* line = prompt();
         char** args = tokenize_line(line);
-        execute_command(args);
+        int status = execute_command(args);
+        // print status with red color text
+        if (status) {
+            printf("\033[31m%d\033[0m ", status);
+        }
         free(line);
         free(args);
     }
